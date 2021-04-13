f15108f6ad35fadc5d4a5f4fe26fe4c3ddf40236
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2021-04-13 21:50:51 +0700

    feat: add isBay format

--- constants/patterns.js
+++ constants/patterns.js
@@ -1,4 +1,5 @@
 const patterns = [
+    `(\\w+\\s){3}\\s[EXP_NAME]\\s=\\s[EXP_ID];`
 ];
 
 module.exports = patterns;
--- functions/file-operation.js
+++ functions/file-operation.js
@@ -4,6 +4,19 @@ const fs = require('graceful-fs');
 
 function modifyRelatedFiles(fileList, patterns, operation) {
     let { currentExperiment } = ExperimentModel;
+
+    fileList.map((file) => {
+        let data = fs.readFileSync(file).toString();
+
+        patterns.map((pattern) => {
+            pattern.replace('[EXP_NAME]', currentExperiment["[EXP_NAME]"]);
+            pattern.replace('[EXP_ID]', currentExperiment["[EXP_ID]"]);
+
+            if (data.search(pattern) !== -1) {
+                console.log(data.replace(pattern, ''));
+            }
+        });
+    });
 }
 
 module.exports = { modifyRelatedFiles };
