80162ccdc451f6b978f3e4a50189cef0b77da6f3
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2021-04-18 03:42:44 +0700

    feat: club seo user and bot experiments into one

--- functions/file-traversal.js
+++ functions/file-traversal.js
@@ -3,60 +3,60 @@ const fs = require('graceful-fs');
 const ExperimentModel = require('../models/experiment-model');
 
 function getExperimentList(filePath, operation, callback) {
-    // var finder = require('findit')(filePath);
-    // let startTime = Date.now();
+    var finder = require('findit')(filePath);
+    let startTime = Date.now();
 
-    // let fileIndex = {};
-    // let expFiles = [];
+    let fileIndex = {};
+    let expFiles = [];
 
-    // finder.on('directory', function (dir, stat, stop) {
-    //     var base = path.basename(dir);
-    //     if (base === '.git' || base === 'node_modules') stop()
-    //     else {
-    //         fileIndex[dir] = 'dir';
-    //     }
-    // });
+    finder.on('directory', function (dir, stat, stop) {
+        var base = path.basename(dir);
+        if (base === '.git' || base === 'node_modules') stop()
+        else {
+            fileIndex[dir] = 'dir';
+        }
+    });
 
-    // finder.on('file', function (file, stat) {
-    //     fileIndex[file] = 'file';
+    finder.on('file', function (file, stat) {
+        fileIndex[file] = 'file';
 
-    //     if (file.toLowerCase().includes('expid')) {
-    //         expFiles.push(file);
-    //     }
-    // });
+        if (file.toLowerCase().includes('expid')) {
+            expFiles.push(file);
+        }
+    });
 
-    // finder.on('link', function (link, stat) {
-    //     fileIndex[link] = 'link';
-    // });
+    finder.on('link', function (link, stat) {
+        fileIndex[link] = 'link';
+    });
 
-    // finder.on('end', () => {
-    //     console.log('Time taken: [File Traversal] ', (Date.now() - startTime) / 1000 + 's');
+    finder.on('end', () => {
+        console.log('Time taken: [File Traversal] ', (Date.now() - startTime) / 1000 + 's');
 
-    //     let fileCount = 0;
-    //     let dirCount = 0;
-    //     let linkCount = 0;
+        let fileCount = 0;
+        let dirCount = 0;
+        let linkCount = 0;
 
-    //     Object.keys(fileIndex).map((el) => {
-    //         switch (fileIndex[el]) {
-    //             case 'file': fileCount++;
-    //                 break;
-    //             case 'dir': dirCount++;
-    //                 break;
-    //             case 'link': linkCount++;
-    //                 break;
-    //         }
-    //     })
+        Object.keys(fileIndex).map((el) => {
+            switch (fileIndex[el]) {
+                case 'file': fileCount++;
+                    break;
+                case 'dir': dirCount++;
+                    break;
+                case 'link': linkCount++;
+                    break;
+            }
+        })
 
-    //     console.log('Directories Count: ', dirCount);
-    //     console.log('Files Count: ', fileCount);
-    //     console.log('Links Count: ', linkCount);
-    //     console.log('Total Experiment Files: ', expFiles.length);
+        console.log('Directories Count: ', dirCount);
+        console.log('Files Count: ', fileCount);
+        console.log('Links Count: ', linkCount);
+        console.log('Total Experiment Files: ', expFiles.length);
 
-    //     ExperimentModel.experimentList = extractExperimentList(expFiles);
-    //     ExperimentModel.fileIndex = fileIndex;
+        ExperimentModel.experimentList = extractExperimentList(expFiles);
+        ExperimentModel.fileIndex = fileIndex;
 
         callback(operation);
-    // });
+    });
 }
 
 function extractExperimentList(expFiles) {
@@ -69,7 +69,16 @@ function extractExperimentList(expFiles) {
             let f1 = data.indexOf('public const string');
             let f2 = data.indexOf(';', f1);
             let substringArr = data.slice(f1, f2).split(' ');
-            expData[substringArr[3]] = substringArr[5];
+            let experimentName = substringArr[3];
+            let experimentId = substringArr[5];
+
+            // Club SEO User and Bot Experiments => One Experiment
+            if (experimentName.slice(-2) === '_U' || experimentName.slice(-2) === '_B') {
+                experimentName = experimentName.slice(0, experimentName.length - 2);
+                experimentId = experimentId.replace('-U', '').replace('-B', '');
+            }
+
+            expData[experimentName] = experimentId;
             data = data.replace(data.slice(f1, f2 + 1), '');
         }
     });
--- index.js
+++ index.js
@@ -10,19 +10,21 @@ getExperimentList(directoryPath, operation, (operation) => {
     // select experiment
     let sampleExperiment = 'PRERENDER_HIDE_HOTEL_MAP';
 
+    console.log(experimentModel.experimentList);
+
     // find affected files
     // let experimentFileList = getExperimentRelatedFiles(sampleExperiment);
 
-    let experimentFileList = [
-        '..\\..\\codebase\\agoda-com-dictator\\Src\\Agoda.Website\\Agoda.Website.MVC.Api\\Constants\\TeamExperiments\\ExpId_Seo.cs',
-        '..\\..\\codebase\\agoda-com-dictator\\Src\\Website.Cronos\\Property\\Agoda.Cronos.Property\\Builders\\ViewModels\\SeoParamsVmBuilder.cs',
-        '..\\..\\codebase\\agoda-com-dictator\\Src\\Website.Cronos\\Property\\Agoda.Cronos.Property\\Constants\\Experiments\\ExpId_Seo.cs',
-        '..\\..\\codebase\\agoda-com-dictator\\Src\\Website.Cronos\\Property\\Agoda.Cronos.PropertyUnitTests\\Builders\\ViewModels\\SeoParamsVmBuilderTest.cs',   
-        '..\\..\\codebase\\agoda-com-dictator\\Src\\Agoda.Website\\Agoda.Website.MVC\\Areas\\NewSite\\Property\\Builders\\PropertyPageParams\\SeoParamsBuilder.cs'
-    ];
+    // let experimentFileList = [
+    //     '..\\..\\codebase\\agoda-com-dictator\\Src\\Agoda.Website\\Agoda.Website.MVC.Api\\Constants\\TeamExperiments\\ExpId_Seo.cs',
+    //     '..\\..\\codebase\\agoda-com-dictator\\Src\\Website.Cronos\\Property\\Agoda.Cronos.Property\\Builders\\ViewModels\\SeoParamsVmBuilder.cs',
+    //     '..\\..\\codebase\\agoda-com-dictator\\Src\\Website.Cronos\\Property\\Agoda.Cronos.Property\\Constants\\Experiments\\ExpId_Seo.cs',
+    //     '..\\..\\codebase\\agoda-com-dictator\\Src\\Website.Cronos\\Property\\Agoda.Cronos.PropertyUnitTests\\Builders\\ViewModels\\SeoParamsVmBuilderTest.cs',   
+    //     '..\\..\\codebase\\agoda-com-dictator\\Src\\Agoda.Website\\Agoda.Website.MVC\\Areas\\NewSite\\Property\\Builders\\PropertyPageParams\\SeoParamsBuilder.cs'
+    // ];
 
     // perform integration/de-integration (Complex)
-    modifyRelatedFiles(experimentFileList, patterns, operation);
+    // modifyRelatedFiles(experimentFileList, patterns, operation);
 
     // create PR
 });
